b0375f4194fbe0e4bbb9210787210e083d778061
code-wave07 <onwudinjopatrick5@gmail.com>
2025-12-02 15:29:37 +0000

    Add files via upload
    built my architecture

---
+++ ChatMeAPI/ChatMe.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage; // Required for IDbContextTransaction
+
+namespace ChatMe.Data.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+
+        Task CommitTransactionAsync();
+
+        DbContext GetDbContext();
+    }
+
+    public interface IUnitOfWork<TContext> : IUnitOfWork where TContext : DbContext
+    {
+    }
+}
