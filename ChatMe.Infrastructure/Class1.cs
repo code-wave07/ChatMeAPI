c580156f141ee56170bb74f3e4f8b9a87c1fc072
Onyekachi <onwudinjopatrick5@gmail.com>
2025-12-02 16:33:28 +0100

    created new classes in models and data

---
+++ ChatMe.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatMe.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ChatMe.Models/Entities/Message.cs
@@ -0,0 +1,29 @@
+﻿using ChatMe.Models.Entities;
+using ChatMe.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChatMe.Models.Entities
+{
+    public class Message
+    {
+        [Key]
+        public string MessageId { get; set; }
+
+        [ForeignKey("Conversation")]
+        public string ConversationId { get; set; }
+        public Conversation Conversation { get; set; }
+
+        // UPDATE: Identity uses String (Guid) IDs
+        [ForeignKey("Sender")]
+        public string SenderId { get; set; }
+        public ApplicationUser Sender { get; set; }
+
+        public string? MessageText { get; set; }
+        public MessageType MessageType { get; set; } = MessageType.Text;
+        public string? MediaUrl { get; set; }
+        public DateTime SentDateTime { get; set; } = DateTime.UtcNow;
+
+        public bool IsDeletedForEveryone { get; set; } = false;
+    }
+}
